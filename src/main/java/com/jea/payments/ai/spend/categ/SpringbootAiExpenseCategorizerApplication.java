065896504fcf789d30a9b453a7cbb86f770bace8
Jagadeesh <blockchian@jeadigital.com>
2024-11-15 08:53:07 +0530

    Initial commit

---
+++ src/main/java/com/jea/payments/ai/spend/categ/SpringbootAiExpenseCategorizerApplication.java
@@ -0,0 +1,13 @@
+package com.jea.payments.ai.spend.categ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAiExpenseCategorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAiExpenseCategorizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jea/payments/ai/spend/categ/SpringbootAiExpenseCategorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jea.payments.ai.spend.categ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAiExpenseCategorizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
